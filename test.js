dd32b18cd60812fcc0939c1bb2a2fc07c6f842f1
jngo2 <james.ngo@toronto.ca>
2021-12-20 12:05:11 -0500

    Add dotenv dependency

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv');
+
 const nodeLoginout = require('./index');
+
+dotenv.config();
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const httpsOptions = {
-	// REMOVED
+	host: process.env.BASE_HOST,
+	path: process.env.BASE_PATH
 };
 
 nodeLoginout.prompt('.login', httpsOptions, 'app').then((loginResult) => {
